fa50fa0479652c12304ac2d266ffff8b718846b7
mqduck <mqduck@mqduck.net>
2020-09-01 15:35:22 -0700

    Add generateAbilities.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {generateAbilities} from "./generateAbilities";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +19,20 @@ function App() {
         >
           Learn React
         </a>
+        {generateAbilities(72, 6, 17, 6, 100)}*/}
+        Ability points
+        <label>
+          Total:
+          <input type="text" name="name"/>
+        </label>
+        Ability score minimum and maximum
+        <label>
+          Minimum:
+          <input type="text" name="name"/>
+          Maximum:
+          <input type="text" name="name"/>
+        </label>
+        <button type="button">Click Me!</button>
       </header>
     </div>
   );
